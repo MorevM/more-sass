96415238a616398e944b227c37d50e4fa9e4aa1a
MorevM <max.seainside@gmail.com>
2022-03-26 21:52:24 +0300

    chore: Deps update

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
 	extends: [
-		'@morev/stylelint-config/scss/warn-autofixable',
+		'@morev/stylelint-config/scss',
 	],
 	rules: {
 		// Well, `stylelint-config` designed to be used with style files,
@@ -8,6 +8,7 @@ module.exports = {
 		'max-line-length': null,
 		'scss/dollar-variable-first-in-block': null,
 		'scss/dollar-variable-empty-line-before': null,
+		'scss/function-no-unknown': null,
 		'length-zero-no-unit': null,
 		'number-leading-zero': null,
 		'aditayvm/at-rule-no-children': null,
